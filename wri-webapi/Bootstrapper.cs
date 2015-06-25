8c34713d8fdbe5bcb5ca05019dfe24124f43ecf9
steveoh <sgourley@utah.gov>
2015-06-24 23:19:25 -0600

    enable cors

--- wri-webapi/Bootstrapper.cs
+++ wri-webapi/Bootstrapper.cs
@@ -1,9 +1,23 @@
-﻿namespace wri_webapi
+﻿using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+
+namespace wri_webapi
 {
     using Nancy;
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            pipelines.AfterRequest += (ctx) =>
+            {
+                ctx.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                ctx.Response.Headers.Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            };
+        }
+
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
