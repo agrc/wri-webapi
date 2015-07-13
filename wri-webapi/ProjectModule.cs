bd4e5265a0ee7ae1516380d78d0fd64d173bba5b
steveoh <sgourley@utah.gov>
2015-07-13 12:42:46 -0600

    update query to match new database schema

--- wri-webapi/ProjectModule.cs
+++ wri-webapi/ProjectModule.cs
@@ -26,30 +26,27 @@ namespace wri_webapi
                                             "p.ProjRegion as region " +
                                             "from PROJECT p where p.Project_ID = @id";
 
-                var spatialQuery = "select 'point' as origin," +
-                                   "pt.FeatureID as featureId," +
-                                   "pt.TypeDescription as type," +
-                                   "pt.SubType,pt.Action,null as size " +
-                                   "from POINT pt where pt.Project_ID = @id " +
-                                   "union select 'line' as origin," +
-                                   "l.FeatureID as id," +
-                                   "l.TypeDescription as type," +
-                                   "l.SubType,l.Action," +
-                                   "cast(round(l.Shape.STLength() * 3.28084,2) as varchar) + ' ft' as size " +
-                                   "from LINE l where l.Project_ID = @id ";
-
-#if DEBUG
-                spatialQuery += "union select 'poly' as origin," +
-                                "p.FeatureID as featureId," +
-                                "p.TypeDescription as Type," +
-                                "a.ActionDescription as SubType," +
-                                "t.TreatmentDescription as Action," +
-                                "cast(round(p.Shape.STArea() * 0.0015625,2) as varchar) + ' mi²' as size " +
-                                "from POLY p " +
-                                "left outer join dbo.ACTION a on p.FeatureID = a.Poly_Id " +
-                                "left outer join dbo.Treatment t on a.Action_ID = t.Action_ID " +
-                                "where Project_ID = @id";
-#endif
+                const string spatialQuery = "select 'point' as origin," +
+                                            "pt.FeatureID as featureId," +
+                                            "pt.TypeDescription as type," +
+                                            "pt.FeatureSubTypeDescription,pt.ActionDescription,null as size " +
+                                            "from POINT pt where pt.Project_ID = @id " +
+                                            "union select 'line' as origin," +
+                                            "l.FeatureID as id," +
+                                            "l.TypeDescription as type," +
+                                            "l.FeatureSubTypeDescription,l.ActionDescription," +
+                                            "cast(round(l.Shape.STLength() * 3.28084,2) as varchar) + ' ft' as size " +
+                                            "from LINE l where l.Project_ID = @id " +
+                                            "union select 'poly' as origin," +
+                                            "p.FeatureID as featureId," +
+                                            "p.TypeDescription as Type," +
+                                            "a.ActionDescription as SubType," +
+                                            "t.TreatmentTypeDescription as Action," +
+                                            "cast(round(p.Shape.STArea() * 0.0015625,2) as varchar) + ' mi²' as size " +
+                                            "from POLY p " +
+                                            "left outer join dbo.AreaACTION a on p.FeatureID = a.FeatureId " +
+                                            "left outer join dbo.AreaTreatment t on a.AreaActionId = t.AreaActionId " +
+                                            "where Project_ID = @id";
 
                 var id = int.Parse(_.id);
 
